f482ff806a44ee4b1c46ac44d72176e096c69e01
Mohamed Hamza <mohamedelwan260@gmail.com>
2026-02-26 04:25:59 +0200

    swing

--- src/BrickBreaker.java
+++ src/BrickBreaker.java
@@ -1,20 +1,7 @@
-import javax.swing.*;
-import  javax.swing.JFrame;
+
 
 public class BrickBreaker {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("BrickBreaker");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(420, 420);
-        frame.setVisible(true);
-
-
-
-//        JFrame frame = new JFrame("Brick Breaker");
-//        frame.setSize(1920, 1080);
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setVisible(true);
+        StartFrame startFrame = new StartFrame();
     }
 }
